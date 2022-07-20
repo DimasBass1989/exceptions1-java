37ccfb88cbb4859c0ee6c04fd10cbc90328a6ee7
Dimas Junior <dimas.junior1989@hotmail.com>
2022-07-20 12:20:13 -0300

    versão menos ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -43,9 +43,17 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for updates must be future dates: ";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "CheckOut date must be after checkin date";
+		}
 		this.checkIn = checkIn;
-		this.checkOut = checkOut;		
+		this.checkOut = checkOut;	
+		return null;
 	}
 	
 	@Override
